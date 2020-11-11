1863a2ebd3cc002c2ceb106268e6cc5bccb930da
Moritz Mahler <moritzmahler45@gmail.com>
2020-11-11 16:21:21 +0100

    basic project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotificationService } from './services/notification.service';
+import { NotificationBannerComponent } from './components/notification-banner/notification-banner.component';
+import { ExampleComponent } from './components/example/example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotificationBannerComponent,
+    ExampleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [NotificationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
